1935c9f3899b78911a163f33da54e9bc544c2204
Ashwitha <107531395+Ashwitha-30@users.noreply.github.com>
2025-08-21 12:30:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("surveyForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent page reload
-
-    alert("Thank you! Your response has been submitted ✅");
-
-    // Reset form after submission
-    document.getElementById("surveyForm").reset();
-});
